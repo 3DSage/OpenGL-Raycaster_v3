84e47f883b01cc113f796ed0e93098f78b9a15b9
Max Rodriguez <maxrcoppola@gmail.com>
2022-02-09 21:22:59 -0700

    linux: Linux support and compiled binary

--- 3DSage_Raycaster_v3.c
+++ 3DSage_Raycaster_v3.c
@@ -15,7 +15,7 @@
 
 float degToRad(float a) { return a*M_PI/180.0;}
 float FixAng(float a){ if(a>359){ a-=360;} if(a<0){ a+=360;} return a;}
-float distance(ax,ay,bx,by,ang){ return cos(degToRad(ang))*(bx-ax)-sin(degToRad(ang))*(by-ay);}
+float distance(float ax, float ay, float bx, float by, float ang){ return cos(degToRad(ang))*(bx-ax)-sin(degToRad(ang))*(by-ay);}
 float px,py,pdx,pdy,pa;
 float frame1,frame2,fps;
 int gameState=0, timer=0; //game state. init, start screen, game loop, win/lose
---
+++ compile.sh
@@ -0,0 +1,2 @@
+# Compile source
+gcc 3DSage_Raycaster_v3.c -lGL -lGLU -lglut -lm -o 3DSage_Raycaster_v3.linux.x86_64
